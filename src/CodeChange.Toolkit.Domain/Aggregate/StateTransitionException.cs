eb995436b759f77cc842ede5016a56afa9812cfe
Craig Bridges <craig@codechange.co.uk>
2019-01-17 16:10:25 +0000

    Added user flow and state transition exceptions

---
+++ src/CodeChange.Toolkit.Domain/Aggregate/StateTransitionException.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeChange.Toolkit.Domain.Aggregate
+{
+    /// <summary>
+    /// Represents an exception for when an invalid entity state transition was attempted
+    /// </summary>
+    public class StateTransitionException : UserFlowException
+    {
+        public StateTransitionException
+            (
+                string message
+            )
+            : base(message)
+        { }
+    }
+}
---
+++ src/CodeChange.Toolkit.Domain/Aggregate/UserFlowException.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeChange.Toolkit.Domain.Aggregate
+{
+    using System;
+    
+    /// <summary>
+    /// Represents an exception for when a user attempted an invalid operation
+    /// </summary>
+    public class UserFlowException : Exception
+    {
+        public UserFlowException
+            (
+                string message
+            )
+            : base(message)
+        { }
+    }
+}
