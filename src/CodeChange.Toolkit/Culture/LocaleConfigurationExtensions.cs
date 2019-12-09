2ac5c7073cdfefa0204ab1626585fc74148d6639
Craig Bridges <craig@codechange.co.uk>
2019-12-09 16:20:41 +0000

    Created get local date extension methods for ILocaleConfiguration

---
+++ src/CodeChange.Toolkit/Culture/LocaleConfigurationExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace CodeChange.Toolkit.Culture
+{
+    using System;
+    
+    /// <summary>
+    /// Represents various extension methods for locale configurations
+    /// </summary>
+    public static class LocaleConfigurationExtensions
+    {
+        /// <summary>
+        /// Gets the local date and time
+        /// </summary>
+        /// <param name="locale">The locale configuration</param>
+        /// <returns>The date and time</returns>
+        public static DateTime GetNowLocal
+            (
+                this ILocaleConfiguration locale
+            )
+        {
+            return DateTime.UtcNow.UtcToLocalTime(locale);
+        }
+
+        /// <summary>
+        /// Gets the local date without the time component
+        /// </summary>
+        /// <param name="locale">The locale configuration</param>
+        /// <returns>The date</returns>
+        public static DateTime GetTodayLocal
+            (
+                this ILocaleConfiguration locale
+            )
+        {
+            return GetNowLocal(locale).Date;
+        }
+    }
+}
