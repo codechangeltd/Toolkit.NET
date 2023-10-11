d6b710796c70f1a75fa990dcf6080220efa2be73
Craig Bridges <craig.bridges@yahoo.co.uk>
2023-10-11 22:44:01 +0100

    Added IDeepCloneable concept

---
+++ src/CodeChange.Toolkit/DeepCloneExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeChange.Toolkit
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class DeepCloneExtensions
+    {
+        /// <summary>
+        /// Produces another list with the same objects deeply cloned using
+        /// their implementation of <see cref="IDeepCloneable{TSelf}"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> DeepClone<T>(this IEnumerable<T> collection) where T : IDeepCloneable<T>
+        {
+            return collection.Select(item => item.DeepClone()).ToList();
+        }
+    }
+}
---
+++ src/CodeChange.Toolkit/IDeepCloneable.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeChange.Toolkit
+{
+    /// <summary>
+    /// <a href="https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern">CRTP</a>
+    /// based interface to implement for objects that can create deep clones of themselves,
+    /// but can be abused if TSelf is not specified as the same type as the implementing class.
+    /// </summary>
+    /// <typeparam name="TSelf"></typeparam>
+    /// <remarks>
+    /// This idea was copied from https://paulsebastian.codes/a-solution-to-deep-cloning-in-csharp
+    /// </remarks>
+    public interface IDeepCloneable<TSelf> where TSelf : IDeepCloneable<TSelf>
+    {
+        public TSelf DeepClone();
+    }
+}
